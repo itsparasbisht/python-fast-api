e8ec3bca6e3c490becef235f40df525a4993d0ef
itsparasbisht <iamparasbisht@gmail.com>
2022-08-22 20:26:23 +0530

    post request

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from xmlrpc.client import Boolean
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -6,3 +8,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World!!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "this is your post"}
+
+@app.post("/post")
+def set_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']}"}
