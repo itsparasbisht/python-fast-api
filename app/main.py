d45138e4f23818e16bc22e4b0af70bff004b8a0c
itsparasbisht <iamparasbisht@gmail.com>
2022-09-17 19:58:39 +0530

    allow cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .db import connect
 from .routers import user, post, auth, vote
 
+origins = ["*"]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_cerdentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 db_res = connect.db_connect()
 
 app.include_router(post.router)
